372962736ec5eb06688e4d0865e1731ffa0521d5
James Maddox <james.maddox07@gmail.com>
2020-03-24 14:44:39 -0400

    added infinite scroll functionality

--- script.js
+++ script.js
@@ -35,5 +35,27 @@ async function showPosts() {
   });
 }
 
+// Show loader and fetch more posts
+function showLoading() {
+  loading.classList.add('show');
+
+  setTimeout(() => {
+    loading.classList.remove('show');
+
+    setTimeout(() => {
+      page++;
+      showPosts();
+    }, 300);
+  }, 1000);
+}
+
 // Show initial posts
 showPosts();
+
+window.addEventListener('scroll', () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading();
+  }
+});
